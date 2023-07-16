6f171cc4fe52b33d726f3ad5731faa1e41a96e3a
Filipp <feelising@gmail.com>
2023-07-16 20:11:24 +0300

    add adaprive

--- src/components/InfiniteBanner/InfiniteBanner.tsx
+++ src/components/InfiniteBanner/InfiniteBanner.tsx
@@ -15,10 +15,13 @@ const InfiniteBanner: React.FC<InfiniteBannerProps> = ({}) => {
       src: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/2560px-Adidas_Logo.svg.png",
     },
     {
-      src: "https://upload.wikimedia.org/wikipedia/ru/thumb/b/b4/Puma_logo.svg/2560px-Puma_logo.svg.png",
+        src: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Logo_Saucony.svg/640px-Logo_Saucony.svg.png",
     },
     {
-        src: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Reebok_delta_logo.svg/640px-Reebok_delta_logo.svg.png",
+      src: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Reebok_delta_logo.svg/640px-Reebok_delta_logo.svg.png",
+    },
+    {
+        src: "https://upload.wikimedia.org/wikipedia/ru/thumb/b/b4/Puma_logo.svg/2560px-Puma_logo.svg.png",
       },
   ];
 
